675662078aaf8ae797201fa5bbd90bd024b57546
alec.tunbridge <alec.tunbridge@autotrader.co.uk>
2019-01-16 09:38:39 +0000

    Petrol first year tax rules implemented.

--- src/test/java/tax/TaxCalculatorPetrolTest.java
+++ src/test/java/tax/TaxCalculatorPetrolTest.java
@@ -1,7 +1,6 @@
 package tax;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -10,7 +9,6 @@ import static java.time.Month.JANUARY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static tax.FuelType.PETROL;
 
-@Ignore
 public class TaxCalculatorPetrolTest {
 
     private static final LocalDate FIRST_OF_JAN_2019 = LocalDate.of(2019, JANUARY, 1);
@@ -18,7 +16,7 @@ public class TaxCalculatorPetrolTest {
 
     @Before
     public void setUp() {
-        taxCalculator = new DummyTaxCalculator();
+        taxCalculator = new TaxCalculator2019();
     }
 
     @Test
