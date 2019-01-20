fddec09202c02bdf334df1b78eff0ab8b9bef099
Caroline Hatwell <caroline.hatwell@coopdigital.co.uk>
2019-01-20 16:54:08 +0000

    fixed expensive car tests, added vehicle class

--- exercises/python/test_tax_calculator_after_first_year_for_expensive_vehicles.py
+++ exercises/python/test_tax_calculator_after_first_year_for_expensive_vehicles.py
@@ -1,5 +1,9 @@
+import datetime
 import unittest
 from dummy_tax_calculator import  DummyTaxCalculator
+from vehicle import Vehicle
+
+
 class TaxCalculatorAfterFirstYearForExpensiveVehiclesTest(unittest.TestCase):
 	def setUp(self):
 		self.tax_calculator = DummyTaxCalculator()
@@ -7,15 +11,15 @@ class TaxCalculatorAfterFirstYearForExpensiveVehiclesTest(unittest.TestCase):
 
 
 	def test_subsequent_years_tax_for_petrol_if_over_40k(self):
-		vehicle = Vehicle(206, "PETROL", FIRST_OF_APRIL_2017, 50000)
-		self.assertEqual(450, tax_calculator.calculate_tax(vehicle))
+		vehicle = Vehicle(206, "PETROL", self.FIRST_OF_APRIL_2017, 50000)
+		self.assertEqual(450, self.tax_calculator.calculate_tax(vehicle))
 
 	def test_subsequent_years_tax_for_electric_if_over_40k(self):
-		vehicle = Vehicle(206, 'ELECTRIC', FIRST_OF_APRIL_2017, 50000)
-		self.assertEqual(310, tax_calculator.calculate_tax(vehicle))
+		vehicle = Vehicle(206, 'ELECTRIC', self.FIRST_OF_APRIL_2017, 50000)
+		self.assertEqual(310, self.tax_calculator.calculate_tax(vehicle))
 
 	def test_subsequent_years_tax_for_alternative_fuel_if_over_40k(self):
-		vehicle = Vehicle(206, "ALTERNATIVE FUEL, FIRST_OF_APRIL_2017, 50000)
-		self.assertEqual(440, tax_calculator.calculate_tax(vehicle))
+		vehicle = Vehicle(206, "ALTERNATIVE FUEL", self.FIRST_OF_APRIL_2017, 50000)
+		self.assertEqual(440, self.tax_calculator.calculate_tax(vehicle))
 
 
