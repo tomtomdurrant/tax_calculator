1ffc8c86ef648d823aa86af2e8b9f894e2aa282d
Tom Durrant <tom_durrant@waters.com>
2020-01-20 13:11:08 +0000

    Refactored using LINQ

--- exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
+++ exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
@@ -1,71 +1,13 @@
-﻿namespace TaxCalculator
+﻿using System.Linq;
+
+namespace TaxCalculator
 {
     public class DefaultTaxCalculator : TaxCalculator
     {
         public override int CalculateTax(Vehicle vehicle)
         {
             var emissions = vehicle.Co2Emissions;
-            if (emissions == 0)
-            {
-                return 0;
-            }
-
-            if (emissions <= 50)
-            {
-                return 10;
-            }
-
-            if (emissions <= 75)
-            {
-                return 25;
-            }
-
-            if (emissions <= 90)
-            {
-                return 105;
-            }
-
-            if (emissions <= 100)
-            {
-                return 125;
-            }
-
-            if (vehicle.Co2Emissions <= 110)
-            {
-                return 145;
-            }
-
-            if (emissions <= 130)
-            {
-                return 165;
-            }
-
-            if (emissions <= 150)
-            {
-                return 205;
-            }
-
-            if (emissions <= 170)
-            {
-                return 515;
-            }
-
-            if (emissions <= 190)
-            {
-                return 830;
-            }
-
-            if (emissions <= 225)
-            {
-                return 1240;
-            }
-
-            if (emissions <=255)
-            {
-                return 1760;
-            }
-
-            return 2070;
+            return StaticEmissions.PetrolEmissions.FirstOrDefault(pair => pair.Key >= emissions).Value;
         }
     }
 }
---
+++ exercises/dotnet/TaxCalculator/StaticEmissions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using static System.Int32;
+
+namespace TaxCalculator
+{
+    public static class StaticEmissions
+    {
+        public static Dictionary<int, int> PetrolEmissions = new Dictionary<int, int>
+        {
+            {0,0},
+            {50, 10},
+            {75, 25},
+            {90, 105},
+            {100, 125},
+            {110, 145},
+            {130, 165},
+            {150, 205},
+            {170, 515},
+            {190, 830},
+            {225, 1240},
+            {255, 1760},
+            {MaxValue, 2070}
+        };
+    }
+}
