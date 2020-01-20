479549f0ba7cbb4965254540dff720ec032b65f5
Tom Durrant <tom_durrant@waters.com>
2020-01-20 11:45:53 +0000

    Passed 0 test

---
+++ exercises/dotnet/TaxCalculator/DefaultTaxCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace TaxCalculator
+{
+    public class DefaultTaxCalculator : TaxCalculator
+    {
+        public override int CalculateTax(Vehicle vehicle)
+        {
+            return 0;
+        }
+    }
+}
