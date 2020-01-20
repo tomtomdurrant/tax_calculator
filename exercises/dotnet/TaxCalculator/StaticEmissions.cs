ecf6e647edfbbaa2bad191d4a5e46506d7b8f3bc
FKANE <FKANE@SBWT618303>
2020-01-20 15:42:20 +0000

    updated Diesel dictionary

--- exercises/dotnet/TaxCalculator/StaticEmissions.cs
+++ exercises/dotnet/TaxCalculator/StaticEmissions.cs
@@ -21,5 +21,22 @@ namespace TaxCalculator
             {255, 1760},
             {MaxValue, 2070}
         };
+
+        public static Dictionary<int, int> DieselEmissionsDictionary = new Dictionary<int, int>
+        {
+            {0, 0},
+            {50, 25},
+            {75, 105},
+            {90, 125},
+            {100, 145},
+            {110, 165},
+            {130, 205},
+            {150, 515},
+            {170, 830},
+            {190, 1240},
+            {225, 1760},
+            {255, 2070},
+            {MaxValue, 2070}
+        };
     }
 }
